045999092a1cc70364635465ff99e004069df54c
22127271 <ntminh22@clc.fitus.edu.vn>
2024-12-21 11:25:58 +0700

    Updated part of the cart model, cart design

--- main-app/models/cartModel.js
+++ main-app/models/cartModel.js
@@ -12,7 +12,7 @@ const cartSchema = new mongoose.Schema({
         {
             product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
             quantity: { type: Number, default: 1 },
-            size: {type: String}
+            size: {type: String, default: "S"}
         }
     ],
     totalPrice: {
